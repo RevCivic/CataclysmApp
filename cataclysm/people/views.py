303e332d625dd0729753a19cf22d72530828714e
RevCivic <mjshank225@gmail.com>
2024-07-20 17:33:13 -0400

    Added People

--- cataclysm/people/views.py
+++ cataclysm/people/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from people.models import Person
+#from people.forms import PersonForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the people index.")
+    people_list = Person.objects.all()
+    order_by = request.GET.get('order_by')
+    if order_by:
+        people_list = people_list.order_by(order_by)
+    context = {
+        'people_list': people_list,
+        
+    }
+    
+    return render(request, 'people_index.html', context)
