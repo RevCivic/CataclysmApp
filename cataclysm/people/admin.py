ee1378999eebaca1cd6ec7c481fda8e7f8ff3db5
RevCivic <mjshank225@gmail.com>
2026-07-20 22:41:10 -0400

    Add import-ready character data schema

--- cataclysm/people/admin.py
+++ cataclysm/people/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Person, Statset, Skillset, PersonImage, Trait
+from .models import (
+    AccommodationAssignment,
+    Capability,
+    OrganizationUnit,
+    Person,
+    PersonAlias,
+    PersonAssignment,
+    PersonCapability,
+    PersonImage,
+    PersonProfileFact,
+    PersonRelationship,
+    Skillset,
+    Statset,
+    Trait,
+)
 
 # Register your models here.
 admin.site.register(Person)
@@ -7,3 +21,11 @@ admin.site.register(Statset)
 admin.site.register(Skillset)
 admin.site.register(PersonImage)
 admin.site.register(Trait)
+admin.site.register(PersonAlias)
+admin.site.register(Capability)
+admin.site.register(PersonCapability)
+admin.site.register(OrganizationUnit)
+admin.site.register(PersonAssignment)
+admin.site.register(PersonRelationship)
+admin.site.register(AccommodationAssignment)
+admin.site.register(PersonProfileFact)
